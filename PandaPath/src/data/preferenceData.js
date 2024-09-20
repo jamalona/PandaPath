3c6c01e8dbdab523448b3be891612976bcc8a4b4
Jamal Onanuga <jamalona17@gmail.com>
2024-09-20 14:17:14 +0100

    small update

---
+++ PandaPath/src/data/preferenceData.js
@@ -0,0 +1,95 @@
+import beijingImg from '../assets/images/tiantianpark.jpg'
+import SouthImg from '../assets/images/tsokaSouth.jpg'
+import Northimg from '../assets/images/'
+import shanghaiImg from '../assets/images/'
+import KongImg from '../assets/images/'
+import tibetImg from '../assets/images/'
+import tibetIm from '../assets/images/'
+import shanghaiIg from '../assets/images/'
+import shanghaiIg from '../assets/images/'
+
+
+
+
+export const itinerary = [
+  {
+    section: "regions",
+    location: [{
+      imglink: beijingImg,
+      title: "Beijing and Northern China",
+    },
+    {
+      imglink: SouthImg,
+      title: "Southwestern China (Chengdu, Yangshou, Guilin, Kunming)",
+    },
+    {
+      imglink: 2,
+      title: "Xi'an and the Northwest Region",
+    },
+    {
+      imglink: 2,
+      title: "The City of Shanghai",
+    },
+    {
+      imglink: 2,
+      title: "Hong Kong & the Pearl River Delta",
+    },
+    {
+      imglink: 2,
+      title: "Tibetan Highlands",
+    },
+    ]
+  },
+  {
+    section: "travelstyle",
+    style: [{
+      imglink: 2,
+      title: "Private tour with dedicated guide",
+    },
+    {
+      imglink: 2,
+      title: "Private transfer, guide for day trips"
+    },
+    {
+      imglink: 2,
+      title: "Self-drive adventure",
+    },
+    {
+      imglink: 2,
+      title: "Pre-arranged Hotels/Transfer, no guide",
+    },
+    ]
+  },
+  {
+    section: "experience",
+    experience: [{
+      imglink: 2,
+      title: "Cultural explorations",
+    },
+    {
+      imglink: 2,
+      title: "Natural scenery and landscapes",
+    },
+    {
+      imglink: 2,
+      title: "Wildlife and animal encounters",
+    },
+    {
+      imglink: 2,
+      title: "Relaxation & leisure",
+    },
+    {
+      imglink: 2,
+      title: "High-end luxury experiences",
+    },
+    {
+      imglink: 2,
+      title: "Exclusive luxury getaways",
+    },
+    {
+      imglink: 2,
+      title: "Soft adventure & exploration",
+    },
+    ]
+  },
+]
